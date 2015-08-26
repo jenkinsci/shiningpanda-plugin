c62f50233e562baeab7c14962473217bedf71dcf
Olivier Mansion <olivier.mansion@shiningpanda.com>
2015-08-26 23:10:49 +0200

    JENKINS-28428: Coverage 4.0 renames status.dat to status.json

--- src/main/java/jenkins/plugins/shiningpanda/actions/coverage/CoverageAction.java
+++ src/main/java/jenkins/plugins/shiningpanda/actions/coverage/CoverageAction.java
@@ -21,9 +21,6 @@
  */
 package jenkins.plugins.shiningpanda.actions.coverage;
 
-import hudson.Util;
-import hudson.model.Action;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -33,26 +30,26 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 
-import jenkins.plugins.shiningpanda.Messages;
-import jenkins.plugins.shiningpanda.publishers.CoveragePublisher;
-
 import org.apache.tools.ant.DirectoryScanner;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.types.FileSet;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
-public abstract class CoverageAction implements Action
-{
+import hudson.Util;
+import hudson.model.Action;
+import jenkins.plugins.shiningpanda.Messages;
+import jenkins.plugins.shiningpanda.publishers.CoveragePublisher;
+
+public abstract class CoverageAction implements Action {
 
     /*
      * (non-Javadoc)
      * 
      * @see hudson.model.Action#getDisplayName()
      */
-    public String getDisplayName()
-    {
-        return Messages.CoverageAction_DisplayName();
+    public String getDisplayName() {
+	return Messages.CoverageAction_DisplayName();
     }
 
     /*
@@ -60,9 +57,8 @@ public abstract class CoverageAction implements Action
      * 
      * @see hudson.model.Action#getUrlName()
      */
-    public String getUrlName()
-    {
-        return CoveragePublisher.BASENAME;
+    public String getUrlName() {
+	return CoveragePublisher.BASENAME;
     }
 
     /*
@@ -70,14 +66,13 @@ public abstract class CoverageAction implements Action
      * 
      * @see hudson.model.Action#getIconFileName()
      */
-    public String getIconFileName()
-    {
-        // Check if folder exists
-        if (show())
-            // If exists display the link
-            return "graph.png";
-        // Else hide the link
-        return null;
+    public String getIconFileName() {
+	// Check if folder exists
+	if (show())
+	    // If exists display the link
+	    return "graph.png";
+	// Else hide the link
+	return null;
     }
 
     /**
@@ -94,16 +89,15 @@ public abstract class CoverageAction implements Action
      *            The request
      * @return The path
      */
-    protected String getPath(StaplerRequest req)
-    {
-        // Get the remaining part of the URL
-        String path = req.getRestOfPath();
-        // Check if nothing left to normalize it
-        if (path.length() == 0)
-            // Normalize it
-            path = "/";
-        // Return the path
-        return path;
+    protected String getPath(StaplerRequest req) {
+	// Get the remaining part of the URL
+	String path = req.getRestOfPath();
+	// Check if nothing left to normalize it
+	if (path.length() == 0)
+	    // Normalize it
+	    path = "/";
+	// Return the path
+	return path;
     }
 
     /**
@@ -113,10 +107,9 @@ public abstract class CoverageAction implements Action
      *            The base directory
      * @return True if contains reports, else false
      */
-    protected boolean hasReports(File base)
-    {
-        // If not null then check that at least one report
-        return base == null ? false : !getReports(base).isEmpty();
+    protected boolean hasReports(File base) {
+	// If not null then check that at least one report
+	return base == null ? false : !getReports(base).isEmpty();
     }
 
     /**
@@ -126,34 +119,32 @@ public abstract class CoverageAction implements Action
      *            The base directory
      * @return The list of relative paths to reports
      */
-    protected List<String> getReports(File base)
-    {
-        // Store reports
-        List<String> dirs = new ArrayList<String>();
-        // Check is base folder was specified
-        if (base == null)
-            // If not return the empty list
-            return dirs;
-        // Check if the base folder is itself a report folder
-        if (isReport(base))
-            // If yes, return it
-            dirs.add("");
-        // Else look for multiple reports
-        else
-        {
-            // Get the file set to search reports
-            FileSet fs = Util.createFileSet(base, "**/" + CoveragePublisher.JS, null);
-            // Get a scanner
-            DirectoryScanner ds = fs.getDirectoryScanner(new Project());
-            // Go threw the found files
-            for (String file : ds.getIncludedFiles())
-                // Check if this is a report
-                if (isReport(new File(base, file).getParentFile()))
-                    // If this is one, add to the list
-                    dirs.add(new File(file).getParent());
-        }
-        // Return the list of relative paths to reports
-        return dirs;
+    protected List<String> getReports(File base) {
+	// Store reports
+	List<String> dirs = new ArrayList<String>();
+	// Check is base folder was specified
+	if (base == null)
+	    // If not return the empty list
+	    return dirs;
+	// Check if the base folder is itself a report folder
+	if (isReport(base))
+	    // If yes, return it
+	    dirs.add("");
+	// Else look for multiple reports
+	else {
+	    // Get the file set to search reports
+	    FileSet fs = Util.createFileSet(base, "**/" + CoveragePublisher.JS, null);
+	    // Get a scanner
+	    DirectoryScanner ds = fs.getDirectoryScanner(new Project());
+	    // Go threw the found files
+	    for (String file : ds.getIncludedFiles())
+		// Check if this is a report
+		if (isReport(new File(base, file).getParentFile()))
+		    // If this is one, add to the list
+		    dirs.add(new File(file).getParent());
+	}
+	// Return the list of relative paths to reports
+	return dirs;
     }
 
     /**
@@ -172,61 +163,56 @@ public abstract class CoverageAction implements Action
      * @throws IOException
      * @throws ServletException
      */
-    protected void serve(StaplerRequest req, StaplerResponse rsp, String title, File base, String path) throws IOException,
-            ServletException
-    {
-        // Check if base folder is specified
-        if (base == null)
-        {
-            // If not send a 404
-            rsp.sendError(HttpServletResponse.SC_NOT_FOUND);
-            // No need to go further
-            return;
-        }
-        // Check if an index generation is required
-        if (!isReport(base) && "/".equals(path))
-        {
-            // Get the reports
-            List<String> dirs = getReports(base);
-            // Create a list of bean to generate page
-            List<Entry> entries = new ArrayList<Entry>();
-            // For each report create an entry
-            for (String dir : dirs)
-                // Create the entry
-                entries.add(new Entry(dir, dir + "/"));
-            // Set the title in context
-            req.setAttribute("title", title);
-            // Set the entries in context
-            req.setAttribute("entries", entries);
-            // Render the page
-            req.getView(CoverageAction.class, "entries.jelly").forward(req, rsp);
-            // No need to go further
-            return;
-        }
-        // Serve a file, check that path is valid
-        if (path.replace('\\', '/').indexOf("/../") != -1)
-        {
-            // If not end a 500
-            rsp.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            // No need to go further
-            return;
-        }
-        // Get the file to serve
-        File file = new File(base, path.substring(1));
-        // Check if this is a folder
-        if (file.isDirectory())
-            // If this is a folder append an index file
-            file = new File(file, CoveragePublisher.INDEX);
-        // Check if this file exists
-        if (!file.exists())
-        {
-            // If not send a 404
-            rsp.sendError(HttpServletResponse.SC_NOT_FOUND);
-            // No need to go further
-            return;
-        }
-        // Else serve the file
-        rsp.serveFile(req, new FileInputStream(file), file.lastModified(), -1, file.length(), file.getName());
+    protected void serve(StaplerRequest req, StaplerResponse rsp, String title, File base, String path)
+	    throws IOException, ServletException {
+	// Check if base folder is specified
+	if (base == null) {
+	    // If not send a 404
+	    rsp.sendError(HttpServletResponse.SC_NOT_FOUND);
+	    // No need to go further
+	    return;
+	}
+	// Check if an index generation is required
+	if (!isReport(base) && "/".equals(path)) {
+	    // Get the reports
+	    List<String> dirs = getReports(base);
+	    // Create a list of bean to generate page
+	    List<Entry> entries = new ArrayList<Entry>();
+	    // For each report create an entry
+	    for (String dir : dirs)
+		// Create the entry
+		entries.add(new Entry(dir, dir + "/"));
+	    // Set the title in context
+	    req.setAttribute("title", title);
+	    // Set the entries in context
+	    req.setAttribute("entries", entries);
+	    // Render the page
+	    req.getView(CoverageAction.class, "entries.jelly").forward(req, rsp);
+	    // No need to go further
+	    return;
+	}
+	// Serve a file, check that path is valid
+	if (path.replace('\\', '/').indexOf("/../") != -1) {
+	    // If not end a 500
+	    rsp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+	    // No need to go further
+	    return;
+	}
+	// Get the file to serve
+	File file = new File(base, path.substring(1));
+	// Check if this is a folder
+	if (file.isDirectory())
+	    // If this is a folder append an index file
+	    file = new File(file, CoveragePublisher.INDEX);
+	// Check if this file exists
+	if (!file.exists()) {
+	    // If not send a 404
+	    rsp.sendError(HttpServletResponse.SC_NOT_FOUND);
+	    // No need to go further
+	    return;
+	}
+	// Else serve the file
+	rsp.serveFile(req, new FileInputStream(file), file.lastModified(), -1, file.length(), file.getName());
     }
 
     /**
@@ -236,49 +222,48 @@ public abstract class CoverageAction implements Action
      *            The folder to check
      * @return True if this is a report, else false
      */
-    protected boolean isReport(File base)
-    {
-        // Check that not null and that the three file exists
-        return base != null && new File(base, CoveragePublisher.INDEX).exists()
-                && new File(base, CoveragePublisher.JS).exists() && new File(base, CoveragePublisher.STATUS).exists();
+    protected boolean isReport(File base) {
+	// Check that not null and that the three file exists
+	return base != null && new File(base, CoveragePublisher.INDEX).exists()
+		&& new File(base, CoveragePublisher.JS).exists()
+		&& (new File(base, CoveragePublisher.STATUS_LTE_3).exists()
+			|| new File(base, CoveragePublisher.STATUS).exists());
     }
 
     /**
      * Bean used to generate page.
      */
-    public class Entry
-    {
-        /**
-         * Children.
-         */
-        public List<Entry> children = new ArrayList<Entry>();
+    public class Entry {
+	/**
+	 * Children.
+	 */
+	public List<Entry> children = new ArrayList<Entry>();
 
-        /**
-         * The title.
-         */
-        public String title;
+	/**
+	 * The title.
+	 */
+	public String title;
 
-        /**
-         * The related link.
-         */
-        public String target;
+	/**
+	 * The related link.
+	 */
+	public String target;
 
-        /**
-         * Constructor using fields.
-         * 
-         * @param title
-         *            The title
-         * @param target
-         *            The link
-         */
-        public Entry(String title, String target)
-        {
-            // Call super
-            super();
-            // Store title
-            this.title = title;
-            // Store target
-            this.target = target;
-        }
+	/**
+	 * Constructor using fields.
+	 * 
+	 * @param title
+	 *            The title
+	 * @param target
+	 *            The link
+	 */
+	public Entry(String title, String target) {
+	    // Call super
+	    super();
+	    // Store title
+	    this.title = title;
+	    // Store target
+	    this.target = target;
+	}
     }
 }
--- src/main/java/jenkins/plugins/shiningpanda/publishers/CoveragePublisher.java
+++ src/main/java/jenkins/plugins/shiningpanda/publishers/CoveragePublisher.java
@@ -21,24 +21,6 @@
  */
 package jenkins.plugins.shiningpanda.publishers;
 
-import hudson.EnvVars;
-import hudson.Extension;
-import hudson.FilePath;
-import hudson.Functions;
-import hudson.Launcher;
-import hudson.Util;
-import hudson.model.Action;
-import hudson.model.BuildListener;
-import hudson.model.Result;
-import hudson.model.AbstractBuild;
-import hudson.model.AbstractProject;
-import hudson.model.Run;
-import hudson.tasks.BuildStepDescriptor;
-import hudson.tasks.BuildStepMonitor;
-import hudson.tasks.Publisher;
-import hudson.tasks.Recorder;
-import hudson.util.FormValidation;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -48,16 +30,32 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 
-import jenkins.plugins.shiningpanda.Messages;
-import jenkins.plugins.shiningpanda.actions.coverage.CoverageBuildAction;
-import jenkins.plugins.shiningpanda.actions.coverage.CoverageProjectAction;
-
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
-public class CoveragePublisher extends Recorder
-{
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Functions;
+import hudson.Launcher;
+import hudson.Util;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.Action;
+import hudson.model.BuildListener;
+import hudson.model.Result;
+import hudson.model.Run;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Publisher;
+import hudson.tasks.Recorder;
+import hudson.util.FormValidation;
+import jenkins.plugins.shiningpanda.Messages;
+import jenkins.plugins.shiningpanda.actions.coverage.CoverageBuildAction;
+import jenkins.plugins.shiningpanda.actions.coverage.CoverageProjectAction;
+
+public class CoveragePublisher extends Recorder {
 
     /**
      * Base name for the HTML report folder on master.
@@ -75,9 +73,14 @@ public class CoveragePublisher extends Recorder
     public final static String INDEX = "index.html";
 
     /**
-     * Status file in report folders.
+     * Status file in report folders for coverage <= 3.
      */
-    public final static String STATUS = "status.dat";
+    public final static String STATUS_LTE_3 = "status.dat";
+
+    /**
+     * Status file in report folders for coverage >= 4.
+     */
+    public final static String STATUS = "status.json";
 
     /**
      * Path to the HTML folder in the workspace.
@@ -91,12 +94,11 @@ public class CoveragePublisher extends Recorder
      *            The HTML directory
      */
     @DataBoundConstructor
-    public CoveragePublisher(String htmlDir)
-    {
-        // Call super
-        super();
-        // Store the HTML directory
-        this.htmlDir = Util.fixEmptyAndTrim(htmlDir);
+    public CoveragePublisher(String htmlDir) {
+	// Call super
+	super();
+	// Store the HTML directory
+	this.htmlDir = Util.fixEmptyAndTrim(htmlDir);
     }
 
     /**
@@ -107,28 +109,28 @@ public class CoveragePublisher extends Recorder
      * @param environment
      *            If environment
      */
-    private List<FilePath> getHtmlDirs(FilePath workspace, EnvVars environment) throws IOException, InterruptedException
-    {
-        // Get the candidates
-        FilePath[] candidates;
-        // If an HTML folder is not specified look for it in all workspace
-        if (htmlDir == null)
-            // List all folders containing a coverage_html.js file
-            candidates = workspace.list("**/" + JS);
-        // Else use the provided value
-        else
-            // List all folders containing a coverage_html.js file
-            candidates = workspace.list(environment.expand(htmlDir) + "/" + JS);
-        // Store the list of matching folders
-        List<FilePath> dirs = new ArrayList<FilePath>();
-        // Go threw the candidates
-        for (FilePath js : candidates)
-            // Check that following files also exists
-            if (js.sibling(INDEX).exists() && js.sibling(STATUS).exists())
-                // Add the parent folder
-                dirs.add(js.getParent());
-        // Return the found directories
-        return dirs;
+    private List<FilePath> getHtmlDirs(FilePath workspace, EnvVars environment)
+	    throws IOException, InterruptedException {
+	// Get the candidates
+	FilePath[] candidates;
+	// If an HTML folder is not specified look for it in all workspace
+	if (htmlDir == null)
+	    // List all folders containing a coverage_html.js file
+	    candidates = workspace.list("**/" + JS);
+	// Else use the provided value
+	else
+	    // List all folders containing a coverage_html.js file
+	    candidates = workspace.list(environment.expand(htmlDir) + "/" + JS);
+	// Store the list of matching folders
+	List<FilePath> dirs = new ArrayList<FilePath>();
+	// Go threw the candidates
+	for (FilePath js : candidates)
+	    // Check that following files also exists
+	    if (js.sibling(INDEX).exists() && (js.sibling(STATUS_LTE_3).exists() || js.sibling(STATUS).exists()))
+		// Add the parent folder
+		dirs.add(js.getParent());
+	// Return the found directories
+	return dirs;
     }
 
     /**
@@ -143,66 +145,61 @@ public class CoveragePublisher extends Recorder
      * @param single
      *            Is there more than one report
      */
-    private FilePath getHtmlTargetDir(FilePath base, FilePath workspace, FilePath dir, boolean single)
-    {
-        // If only one report, copy it in the base folder
-        if (single)
-            // Return the base folder
-            return base;
-        // Else replicate the workspace tree. Get the workspace path.
-        String ws = workspace.getRemote();
-        // Get the index to delete the workspace path
-        int beginIndex = ws.endsWith("/") || ws.endsWith("\\") ? ws.length() : ws.length() + 1;
-        // Delete workspace part
-        return base.child(dir.getRemote().substring(beginIndex));
+    private FilePath getHtmlTargetDir(FilePath base, FilePath workspace, FilePath dir, boolean single) {
+	// If only one report, copy it in the base folder
+	if (single)
+	    // Return the base folder
+	    return base;
+	// Else replicate the workspace tree. Get the workspace path.
+	String ws = workspace.getRemote();
+	// Get the index to delete the workspace path
+	int beginIndex = ws.endsWith("/") || ws.endsWith("\\") ? ws.length() : ws.length() + 1;
+	// Delete workspace part
+	return base.child(dir.getRemote().substring(beginIndex));
     }
 
     /*
      * (non-Javadoc)
      * 
-     * @see
-     * hudson.tasks.BuildStepCompatibilityLayer#perform(hudson.model.AbstractBuild
-     * , hudson.Launcher, hudson.model.BuildListener)
+     * @see hudson.tasks.BuildStepCompatibilityLayer#perform(hudson.model.
+     * AbstractBuild , hudson.Launcher, hudson.model.BuildListener)
      */
     @Override
-    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException,
-            IOException
-    {
-        // Get the workspace
-        FilePath workspace = build.getWorkspace();
-        // Look for HTML reports
-        List<FilePath> dirs = getHtmlDirs(build.getWorkspace(), build.getEnvironment(listener));
-        // If no reports, log and modify build result
-        if (dirs.size() == 0)
-        {
-            // If build is already unstable log nothing
-            if (build.getResult().isBetterOrEqualTo(Result.UNSTABLE))
-                // Else log that nothing found
-                listener.error(Messages.CoverageArchiver_HtmlDir_NotFound());
-            // Set build as failure
-            build.setResult(Result.FAILURE);
-            // Go on
-            return true;
-        }
-        // Get the base target folder
-        FilePath base = new FilePath(getHtmlDir(build));
-        // Cleanup
-        base.deleteRecursive();
-        // Go threw the report folders
-        for (FilePath dir : dirs)
-        {
-            // Get the target folder
-            FilePath targetDir = getHtmlTargetDir(base, workspace, dir, dirs.size() == 1);
-            // Only copy files if not already exists, we do not handle included
-            // reports
-            if (!targetDir.exists())
-                // Copy their contents
-                dir.copyRecursiveTo("**/*", targetDir);
-        }
-        // Add the build action
-        build.addAction(new CoverageBuildAction(build));
-        // Go on
-        return true;
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
+	    throws InterruptedException, IOException {
+	// Get the workspace
+	FilePath workspace = build.getWorkspace();
+	// Look for HTML reports
+	List<FilePath> dirs = getHtmlDirs(build.getWorkspace(), build.getEnvironment(listener));
+	// If no reports, log and modify build result
+	if (dirs.size() == 0) {
+	    // If build is already unstable log nothing
+	    if (build.getResult().isBetterOrEqualTo(Result.UNSTABLE))
+		// Else log that nothing found
+		listener.error(Messages.CoverageArchiver_HtmlDir_NotFound());
+	    // Set build as failure
+	    build.setResult(Result.FAILURE);
+	    // Go on
+	    return true;
+	}
+	// Get the base target folder
+	FilePath base = new FilePath(getHtmlDir(build));
+	// Cleanup
+	base.deleteRecursive();
+	// Go threw the report folders
+	for (FilePath dir : dirs) {
+	    // Get the target folder
+	    FilePath targetDir = getHtmlTargetDir(base, workspace, dir, dirs.size() == 1);
+	    // Only copy files if not already exists, we do not handle included
+	    // reports
+	    if (!targetDir.exists())
+		// Copy their contents
+		dir.copyRecursiveTo("**/*", targetDir);
+	}
+	// Add the build action
+	build.addAction(new CoverageBuildAction(build));
+	// Go on
+	return true;
     }
 
     /*
@@ -213,10 +210,9 @@ public class CoveragePublisher extends Recorder
      * .AbstractProject)
      */
     @Override
-    public Collection<Action> getProjectActions(AbstractProject<?, ?> project)
-    {
-        // Return the project action
-        return Collections.<Action> singleton(new CoverageProjectAction(project));
+    public Collection<Action> getProjectActions(AbstractProject<?, ?> project) {
+	// Return the project action
+	return Collections.<Action> singleton(new CoverageProjectAction(project));
     }
 
     /*
@@ -224,9 +220,8 @@ public class CoveragePublisher extends Recorder
      * 
      * @see hudson.tasks.BuildStep#getRequiredMonitorService()
      */
-    public BuildStepMonitor getRequiredMonitorService()
-    {
-        return BuildStepMonitor.NONE;
+    public BuildStepMonitor getRequiredMonitorService() {
+	return BuildStepMonitor.NONE;
     }
 
     /**
@@ -236,69 +231,63 @@ public class CoveragePublisher extends Recorder
      *            The build
      * @return The path to the HTML folder
      */
-    public static File getHtmlDir(Run<?, ?> run)
-    {
-        // Check if the provided run exists
-        return run == null ? null : new File(run.getRootDir(), BASENAME);
+    public static File getHtmlDir(Run<?, ?> run) {
+	// Check if the provided run exists
+	return run == null ? null : new File(run.getRootDir(), BASENAME);
     }
 
     /**
      * Recorder descriptor.
      */
     @Extension
-    public static class DescriptorImpl extends BuildStepDescriptor<Publisher>
-    {
-        /*
-         * (non-Javadoc)
-         * 
-         * @see hudson.model.Descriptor#getDisplayName()
-         */
-        public String getDisplayName()
-        {
-            return Messages.CoverageArchiver_DisplayName();
-        }
+    public static class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see hudson.model.Descriptor#getDisplayName()
+	 */
+	public String getDisplayName() {
+	    return Messages.CoverageArchiver_DisplayName();
+	}
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see hudson.model.Descriptor#getHelpFile()
-         */
-        @Override
-        public String getHelpFile()
-        {
-            return Functions.getResourcePath() + "/plugin/shiningpanda/help/publishers/CoveragePublisher/help.html";
-        }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see hudson.model.Descriptor#getHelpFile()
+	 */
+	@Override
+	public String getHelpFile() {
+	    return Functions.getResourcePath() + "/plugin/shiningpanda/help/publishers/CoveragePublisher/help.html";
+	}
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see hudson.tasks.BuildStepDescriptor#isApplicable(java.lang.Class)
-         */
-        public boolean isApplicable(@SuppressWarnings("rawtypes") Class<? extends AbstractProject> jobType)
-        {
-            // Always applicable
-            return true;
-        }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see hudson.tasks.BuildStepDescriptor#isApplicable(java.lang.Class)
+	 */
+	public boolean isApplicable(@SuppressWarnings("rawtypes") Class<? extends AbstractProject> jobType) {
+	    // Always applicable
+	    return true;
+	}
 
-        /**
-         * 
-         * Check if the HTML folder is relative to the workspace.
-         * 
-         * @param project
-         *            The project
-         * @param value
-         *            The value to check
-         * @return Success if the value is relative
-         * @throws IOException
-         * @throws ServletException
-         */
-        public FormValidation doCheckHtmlDir(@AncestorInPath AbstractProject<?, ?> project, @QueryParameter String value)
-                throws IOException, ServletException
-        {
-            // Get a workspace
-            FilePath workspace = project.getSomeWorkspace();
-            // If a workspace is available, check that the value is relative
-            return workspace != null ? workspace.validateFileMask(value) : FormValidation.ok();
-        }
+	/**
+	 * 
+	 * Check if the HTML folder is relative to the workspace.
+	 * 
+	 * @param project
+	 *            The project
+	 * @param value
+	 *            The value to check
+	 * @return Success if the value is relative
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	public FormValidation doCheckHtmlDir(@AncestorInPath AbstractProject<?, ?> project,
+		@QueryParameter String value) throws IOException, ServletException {
+	    // Get a workspace
+	    FilePath workspace = project.getSomeWorkspace();
+	    // If a workspace is available, check that the value is relative
+	    return workspace != null ? workspace.validateFileMask(value) : FormValidation.ok();
+	}
     }
 }
